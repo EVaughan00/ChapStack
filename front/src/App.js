7ec2d96a8a22065bfc806e447d051ab471de0176
EVaughan00 <evanvaughan9@gmail.com>
2019-04-29 10:48:17 -0600

    React implemented

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Login from "./components/login";
+import Register from "./components/register";
+import Users from "./components/users";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/" className="navbar-brand">Evan's MERN Stack WebApp</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav ml-auto">
+              <li className="navbar-item">
+                <Link to="/login" className="nav-link">Login</Link>
+              </li>
+                <li className="navbar-item">
+                  <Link to="/register" className="nav-link mr-auto">Register</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/users" className="nav-link mr-auto">List Users</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <Route path="/login" exact component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/users" component={Users} />
+        </div>
+      </Router>
     </div>
   );
 }
